c9ceb32225eb5053586d001b97007a17b48b7734
vitorialmdd <vitorialmdd@gmail.com>
2022-10-21 20:54:41 -0300

    project first commit

---
+++ src/rules.js
@@ -0,0 +1,16 @@
+
+
+// visa ^4\d{0,15}
+// inicia com 4 seguido de mais 15 digitos
+4234234234234244
+
+//master (^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}
+// inicia com 5, seguido de um digito entre 1 e 5, seguido de mais dois digitos
+//OU
+//inicia com 22, seguido de um digito entre 2 e 9, seguido de mais 1 digito
+//OU
+// inicia com 2, seguido de um digito entre 3 e 7, seguido de mais 2 digitos
+// seguido de mais 12 digitos
+5353535353535353
+2323232323232323
+2234456788765734 
